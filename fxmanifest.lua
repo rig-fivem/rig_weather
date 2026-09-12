0613abc7fbaad667ba658f611afdf28a6f13f2c0
Case <caseirl@mail.com>
2026-09-12 16:14:22 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,7 +11,7 @@ License: https://github.com/rig-fivem/rig_weather/blob/main/LICENSE
 fx_version "cerulean"
 games { "gta5" }
 name "rig_weather"
-version "0.1.0"
+version "0.2.0"
 description "Persistent seasonal weather system for RIG (FiveM)."
 license "Apache 2.0"
 author "Case"
